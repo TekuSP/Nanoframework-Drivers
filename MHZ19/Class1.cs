9977344e89037bf1ce1e23da7063e49c82779d7f
Richard Torhan <richard.torhan@windowslive.com>
2021-02-09 23:02:00 +0100

    Update nugets to date, fix many issues with Visual Studio, add new projects

---
+++ MHZ19/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MHZ19
+{
+    public class MHZ19
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
---
+++ SSD1331/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SSD1331
+{
+    public class SSD1331
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
