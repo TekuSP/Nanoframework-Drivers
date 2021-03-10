d69095ca8dbea2b294a55e382d81becd6c360a9d
TekuSP <richard.torhan@windowslive.com>
2021-03-10 23:21:21 +0100

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -9,8 +9,7 @@ $idVS2019 = 1
 
 $VsPath = $(&$VsWherePath -latest -property installationPath)
 
-$msbuildPath = Join-Path -Path $VsPath -ChildPath "\MSBuild"
-
+$msbuildPath = Join-Path -Path $VsPath -ChildPath "\MSBuild\Current\Bin\"
 Write-Output "Found MSBUILD in: $msbuildPath\msbuild.exe"
 Write-Output "Restore $Solution_Name /t:Restore /p:Configuration=$Configuration -m"
 &"$msbuildPath\msbuild.exe" "$Solution_Name /t:Restore /p:Configuration=$Configuration -m" | Write-Output
