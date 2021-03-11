c97355a70e98eb753676c9ffdaef5c8d555121f4
TekuSP <richard.torhan@windowslive.com>
2021-03-11 18:37:30 +0100

    SHTC3 Initial commit

---
+++ Drivers/SHTC3/SHTC3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SHTC3
+{
+    public class SHTC3
+    {
+        // Browse our samples repository: https://github.com/nanoframework/samples
+        // Check our documentation online: https://docs.nanoframework.net/
+        // Join our lively Discord community: https://discord.gg/gCyBu8T
+    }
+}
