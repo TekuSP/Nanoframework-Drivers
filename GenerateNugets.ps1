ee81d2123226e01f80ae859b0fc559b36eeaaca3
TekuSP <richard.torhan@windowslive.com>
2021-03-12 16:34:34 +0100

    Fix last assembly and add nuget generating script

---
+++ GenerateNugets.ps1
@@ -0,0 +1,8 @@
+﻿$nugetsToProcess = (Get-ChildItem -Path "." -Filter *.nuspec -r | % { echo $_.FullName });
+Foreach ($nuget in $nugetsToProcess)
+{
+    echo "-----------------------------------";
+    echo "Packing nuget file: $nuget";
+    &"nuget.exe" "pack" "$nuget" | Write-Host;
+    echo "Finished packing nuget file: $nuget";
+}
